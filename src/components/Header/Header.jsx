ce0e73eb3ea4e11e2ef0870a6a01367c7ce1d7f1
Rifat <mondolrobi@gmail.com>
2023-04-05 07:59:54 +0600

    header complete for desktop

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './/Header.css';
+import logo from '../../images/Logo.svg';
 
 const Header = () => {
     return (
-        <div>
-            <h2 className='ema'> 
-                Ema-John
-            </h2>
+        <div className='header'>
+            <img className='ema-logo' src={logo} alt="" />
+
+           <div className='menu'>
+            <a href="/order">Order</a>
+            <a href="/shop">Shop</a>
+            <a href="/inventory">Inventory</a>
+            <a href="Login">Login</a>
+           </div>
         </div>
     );
 };
